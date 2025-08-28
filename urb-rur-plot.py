48d53e6a10ad91d60b2b737083886709fd93a5d5
Aadya Bhardwaj <bhardwaj.aady@gmail.com>
2025-08-28 17:52:37 +0530

    Add files via upload

---
+++ urb-rur-plot.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r"C:\Users\Aadya Bhardwaj\.ugs\project\sorted_urban-rural.csv")
+
+x = df['OFC Density']
+y = df['% Rural Households']
+
+plt.scatter(x, y)    
+plt.xlabel('OFC Density')
+plt.ylabel('% Rural Households')
+plt.title('Plot of OFC Density vs % Rural Households')
+plt.show()
